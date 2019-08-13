331aacefae2ca2e2111d1a3170828fe35ed3a246
Anthony Cerruti <soren@disroot.org>
2019-08-12 21:41:56 -0700

    Move some header methods to the source file

--- source/script_provider.hpp
+++ source/script_provider.hpp
@@ -40,16 +40,10 @@ public:
         return internalQueue.size();
     }
 
-    std::shared_ptr<struct controlMsg> nextLine()
-    {
-        return pullFromQueue();
-    }
-    bool hasNextLine()
-    {
-        return queueIsEmpty();
-    }
+    virtual std::shared_ptr<struct controlMsg> nextLine();
+    virtual bool hasNextLine();
 
-    virtual void populateQueue() {};
+    virtual void populateQueue();
 
 };
 
@@ -71,29 +65,8 @@ public:
     : stream(mStream)
     {}
 
-    bool hasNextLine()
-    {
-        return !(stream.eof() && queueIsEmpty());
-    }
-    void populateQueue()
-    {
-        if(shouldPopulate())
-        {
-            int frame;
-            std::string keyStr, lStickStr, rStickStr;
-            while(queueSize() < 30 && !stream.eof())
-            {
-                stream >> frame >> keyStr >> lStickStr >> rStickStr;
-                struct controlMsg msg = lineAsControlMsg(frame, keyStr, lStickStr, rStickStr);
-                std::shared_ptr<struct controlMsg> sharedPtr = std::make_shared<struct controlMsg>(msg);
-                pushToQueue(sharedPtr);
-            }
-            if(stream.eof())
-            {
-                afterEOF();
-            }
-        }
-    }
+    bool hasNextLine();
+    void populateQueue();
 
 };
 
