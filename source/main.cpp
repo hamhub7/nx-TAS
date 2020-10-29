6af101824327f25ec987ec4763ad7f3a96d7faca
io mintz <gie9ohbeixah@paperboats.net>
2020-10-29 04:06:07 +0000

    add license headers to all files
    This way, anyone copying the code will also copy a reference to the license,
    unless they take effort to remove it.

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 // Include the most common headers from the C standard library
 #include <stdio.h>
 #include <stdlib.h>
--- source/script_init.cpp
+++ source/script_init.cpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include <cstring>
 #include <cstdlib>
 #include <cstdint>
